a252a8085a77a5b73c4b90628929eb36826b8a8b
Shiva Ramakrishnan <112168234+ShivaSRK2002@users.noreply.github.com>
2024-09-27 16:08:27 +0530

    Add files via upload

---
+++ Portfolio/main.js
@@ -0,0 +1,9 @@
+// Function to toggle responsive navigation
+function toggleMenu() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');
+}
+// Function to toggle dark mode
+function toggleTheme() {
+    document.body.classList.toggle("dark-mode"); // Adds or removes 'dark-mode' class
+}
